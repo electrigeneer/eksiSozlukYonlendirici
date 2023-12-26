c2af6c15eaf850b49f126bb0148e13d5dc1bdb87
Electrigeneer <137040325+electrigeneer@users.noreply.github.com>
2023-12-26 14:31:54 +0300

    eksiSozluk link update

--- eksiSozlukYonlendirici.js
+++ eksiSozlukYonlendirici.js
@@ -2,7 +2,7 @@
 // @name         Eskisozluk URL yönlendirici
 // @namespace    electrigeneer
 // @version      0.1
-// @description  Google üzerindeki eksisozluk1923.com adresine tıklandığında otomatik olarak eksisozluk1999.com adresine yönlendirir
+// @description  Google üzerindeki eksisozluk1999.com adresine tıklandığında otomatik olarak eksisozluk111.com adresine yönlendirir
 // @match        *://www.google.com/*
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?domain=eksisozluk.com
@@ -14,10 +14,10 @@
     document.addEventListener('click', function(event) {
         var link = event.target.closest('a');
         // Özelleştirilebilir URL
-        if (link && link.href.includes('eksisozluk1923.com')) {
+        if (link && link.href.includes('eksisozluk1999.com')) {
             event.preventDefault();
             // Özelleştirilebilir URL
-            var yeniURL = link.href.replace('eksisozluk1923.com', 'eksisozluk1999.com');
+            var yeniURL = link.href.replace('eksisozluk1999.com', 'eksisozluk111.com');
             window.location.href = yeniURL;
         }
     }, true);
